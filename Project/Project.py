f6e6a6bf8d4d85c8a1dbb5949c844ba9b2808ad6
ToniRajamaki <zingoo16@gmail.com>
2020-12-01 11:35:24 +0200

    mfcc, std and mean are now done

--- Project/Project.py
+++ Project/Project.py
@@ -8,7 +8,10 @@ from scipy.io import wavfile
 from scipy.fftpack import fft
 import csv
 import librosa
+import librosa.display
+import IPython.display as ipd
 CSVname = 'H283175.csv'
+NUM_OF_FILTERS = 40
 
 #Creates a dictionary from given csv file in form [Audiofilename : [ list, of ,tags ] ]
 def ReadCsvToDict(CSVname):
@@ -26,19 +29,31 @@ def ReadCsvToDict(CSVname):
             files[newstr] = tag_list
     return files
 
-# def GetWavWithPath(path):
+#Calculates averages and stds for each row in Mfccs and then combines them : [ ListOfMeans , ListOfStd ]
+def CalcMeanAndStd(mfccs):
+    averages = []
+    stds = []
+    for i in range(NUM_OF_FILTERS):
+        mean_value = np.mean(mfccs[i])
+        averages.append(mean_value)
+        std_value = np.std(mfccs[i])
+        stds.append(std_value)
+
+    mean_and_std = averages + stds
+    return mean_and_std
+
 
 files = ReadCsvToDict(CSVname)
 print(files)
 for fileName in files:
     path = 'audio_files/'+fileName
-    (signal, rate) = librosa.load(path, sr=None)
+    (signal, sr) = librosa.load(path, sr=None)
+    mfccs = librosa.feature.mfcc(y=signal, n_mfcc=NUM_OF_FILTERS, sr=sr)
+    mean_and_stds = CalcMeanAndStd(mfccs)
+
+    print(mean_and_stds)
     break
 
-plt.figure()
-plt.plot(signal)
-plt.title(rate)
-plt.show()
 
 
 
