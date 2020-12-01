5413b0e8e4944688a345d33627ff986e03696df8
ToniRajamaki <zingoo16@gmail.com>
2020-12-01 12:19:09 +0200

    Step 1 supposudely done

--- Project/Project.py
+++ Project/Project.py
@@ -7,18 +7,27 @@ import time
 from scipy.io import wavfile
 from scipy.fftpack import fft
 import csv
+
+from numpy import dot
+from numpy.linalg import norm
 import librosa
 import librosa.display
 import IPython.display as ipd
+from scipy.spatial import distance
 CSVname = 'H283175.csv'
 NUM_OF_FILTERS = 40
+mean_n_std_map = {}
 
 #Creates a dictionary from given csv file in form [Audiofilename : [ list, of ,tags ] ]
+files_n = 11
 def ReadCsvToDict(CSVname):
     files = {}
+    counter = 0
     with open(CSVname, 'r') as data:
         for line in csv.DictReader(data):
-
+            counter = counter + 1
+            if counter == files_n:
+                break
 
             mystr = line['fileName']
             # Changing file ending from .mp3 to .wav
@@ -43,17 +52,36 @@ def CalcMeanAndStd(mfccs):
     return mean_and_std
 
 
+def CalcSimiliratyMatrix(mean_n_std_map):
+
+    simularityMatrix = np.zeros((len(mean_n_std_map),len(mean_n_std_map)))
+    row = 0
+
+
+    for i in mean_n_std_map:
+        column = 0
+        print("row: ",row)
+        for j in mean_n_std_map:
+            #calculate Cosine Similarity
+            cos_sim = dot(mean_n_std_map[i], mean_n_std_map[j]) / (norm(mean_n_std_map[i]) * norm(mean_n_std_map[j]))
+            simularityMatrix[row, column] =cos_sim
+
+            print(column)
+            column = column + 1
+        row = row + 1
+    return simularityMatrix
 files = ReadCsvToDict(CSVname)
-print(files)
+
 for fileName in files:
     path = 'audio_files/'+fileName
     (signal, sr) = librosa.load(path, sr=None)
     mfccs = librosa.feature.mfcc(y=signal, n_mfcc=NUM_OF_FILTERS, sr=sr)
     mean_and_stds = CalcMeanAndStd(mfccs)
+    mean_n_std_map[fileName] = mean_and_stds
 
-    print(mean_and_stds)
-    break
 
+sim_matrix = CalcSimiliratyMatrix(mean_n_std_map)
+print(sim_matrix)
 
 
 
